4bcbc24f53bbba70caf2358ddcd003382900ed73
Summer Kim <sunmer6@gmail.com>
2021-10-20 18:10:45 +0900

    초기 세팅 오류 해결

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Nav from "./components/Nav";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
 
 // 보라's 컴포넌트
-import LoginBora from "./pages/BoraPark/Login/Login";
-import MainBora from "./pages/BoraPark/Main/Main";
+import LoginBora from './pages/BoraPark/Login/Login';
+import MainBora from './pages/BoraPark/Main/Main';
 // 창민's 컴포넌트
-import LoginChangmin from "./pages/ChangminJeon/Login/Login";
-import MainChangmin from "./pages/ChangminJeon/Main/Main";
+import LoginChangmin from './pages/ChangminJeon/Login/Login';
+import MainChangmin from './pages/ChangminJeon/Main/Main';
 // 여름's 컴포넌트
-import LoginSummer from "./pages/SummerKim/Login/Login";
-import MainSummer from "./pages/SummerKim/Main/Main";
+import LoginSummer from './pages/SummerKim/Login/Login';
+import MainSummer from './pages/SummerKim/Main/Main';
 // 원철's 컴포넌트
-import LoginWoncheol from "./pages/WoncheolOk/Login/Login";
-import MainWoncheol from "./pages/WoncheolOk/Main/Main";
+import LoginWoncheol from './pages/WoncheolOk/Login/Login';
+import MainWoncheol from './pages/WoncheolOk/Main/Main';
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 export class Routes extends Component {
