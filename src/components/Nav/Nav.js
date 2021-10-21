ef52b869c3fffa50f4a7901495c25f4c5ad72fac
standup8 <borapark13@gmail.com>
2021-10-21 16:49:31 +0900

    Add: 공통 Nav.js, Nav.scss 적용

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import '../Nav/Nav.scss';
 
-export class Login extends Component {
+class Nav extends Component {
   render() {
     return (
-      <div>
-        <div>Hello</div>
-      </div>
+      <header>
+        <nav>
+          <div className="logo clearfix">
+            <a href="#" className="logoImg">
+              <img src="/images/BoraPark/logo.png" alt="Westagram logo" />
+            </a>
+
+            <h1 className="logoTxt eng">
+              <a href="#">Westagram</a>
+            </h1>
+          </div>
+
+          <div className="search-box">
+            <input type="text" placeholder="검색" />
+          </div>
+
+          <div className="icon-wrap">
+            <a href="#">
+              <img src="/images/BoraPark/ico_explore.png" alt="explore" />
+            </a>
+            <a href="#">
+              <img src="/images/BoraPark/ico_heart.png" alt="heart" />
+            </a>
+            <a href="#">
+              <img src="/images/BoraPark/ico_profile.png" alt="profile" />
+            </a>
+          </div>
+        </nav>
+      </header>
     );
   }
 }
-
-export default Login;
+export default withRouter(Nav);
--- src/pages/BoraPark/Login/Login.js
+++ src/pages/BoraPark/Login/Login.js
@@ -1,13 +1,49 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import './Login.scss';
 
-export class Nav extends Component {
+class Login extends Component {
+  matchValue = elem => {
+    const id = document.getElementById('userId');
+    const pw = document.getElementById('userPw');
+    const btn = document.querySelector('button');
+    if (!id.value || !pw.value) {
+      btn.style.opacity = 0.4;
+    } else {
+      btn.style.opacity = 1;
+    }
+  };
+  goToMain = () => {
+    this.props.history.push('/main');
+  };
   render() {
     return (
-      <div>
-        <div>Hello</div>
+      <div id="wrapper">
+        <h1 className="logo eng">
+          <a href="#">Westagram</a>
+        </h1>
+
+        <form className="loginFrm">
+          <input
+            onChange={this.matchValue}
+            type="text"
+            id="userId"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            onChange={this.matchValue}
+            type="password"
+            id="userPw"
+            placeholder="비밀번호"
+          />
+          <button onClick={this.goToMain} type="submit" className="loginBtn">
+            로그인
+          </button>
+        </form>
+
+        <p>비밀번호를 잊으셨나요?</p>
       </div>
     );
   }
 }
-
-export default Nav;
+export default Login;
