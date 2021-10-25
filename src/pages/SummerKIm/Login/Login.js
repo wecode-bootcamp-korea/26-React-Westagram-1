2805f8ae9afbef9f7ee63ef8930868b257358b29
Summer Kim <sunmer6@gmail.com>
2021-10-25 19:45:47 +0900

    Refactoring: login page 진행중

--- src/pages/SummerKIm/Login/Login.js
+++ src/pages/SummerKIm/Login/Login.js
@@ -7,49 +7,25 @@ export class Login extends Component {
     this.state = {
       userId: '',
       userPw: '',
-      opacity: false,
-      type: 'password',
-      color: true,
     };
-    this.userIdRef = React.createRef();
-    this.userPwRef = React.createRef();
-    this.buttonRef = React.createRef();
   }
 
-  handleIdInput = e => {
-    this.setState({ userId: e.target.value, color: true, value: false });
-    console.log(this.state.userId);
-  };
-
-  handlePwInput = e => {
-    if (!this.state.userId.includes('@')) {
-      // 아이디를 이메일 형식으로 썼는지 확인
-      this.setState({ color: false, value: false });
-      // this.userIdRef.current.value = '이메일 형식으로 입력해주세요';
-      // this.userIdRef.current.style.color = false; // FIXME: red로 바꾼 값 원상복귀
-    } else {
-      this.setState({
-        userPw: e.target.value,
-        opacity: true,
-        color: true,
-        disabled: false,
-      });
-    }
+  handleInput = e => {
+    console.log(e);
+    this.setState({ [e.target.name]: e.target.value });
+    // FIXME: 정규표현식 사용
   };
 
   handleOnClick = () => {
-    if (this.state.userPw.length < 5) {
-      this.userPwRef.current.type = 'text'; // type 값 변경해서 메세지 뜰 수 있게
-      this.userPwRef.current.value = '비밀번호를 5자 이상 입력하세요';
-      this.userPwRef.current.style.color = 'red'; // FIXME: red로 바꾼 값 원상복귀
-      return;
-    }
     if (this.state.userId.length > 1 && this.state.userPw.length > 1) {
       alert('로그인 되었습니다.');
     }
   };
 
   render() {
+    const idVar = this.state.userId.includes('@');
+    const pwVar = this.state.userPw.length > 5;
+
     return (
       <main className="main">
         <div className="container">
@@ -59,26 +35,27 @@ export class Login extends Component {
               type="text"
               id="id"
               placeholder="전화번호, 사용자 이름 또는 이메일"
-              onChange={this.handleIdInput}
-              ref={this.userIdRef}
-              style={{ color: this.state.color ? 'black' : 'red' }}
+              name="userId"
+              onChange={this.handleInput}
+              value={this.state.userId}
+              style={{ color: idVar ? 'black' : 'red' }}
             />
             <input
-              type={this.state.type}
               id="pw"
+              type="password"
               placeholder="비밀번호"
-              onChange={this.handlePwInput}
-              ref={this.userPwRef}
-              style={{ color: this.state.color ? 'black' : 'red' }}
+              name="userPw"
+              value={this.state.userPw}
+              onChange={this.handleInput}
+              style={{ color: pwVar ? 'black' : 'red' }}
             />
             <button
               id="loginBtn"
               onClick={this.handleOnClick}
-              ref={this.buttonRef}
               style={{
-                opacity: this.state.opacity ? '1' : '0.4',
+                opacity: idVar && pwVar ? '1' : '0.4',
               }}
-              disabled={!this.state.userPw && !this.state.userId}
+              disabled={!idVar && pwVar}
             >
               로그인
             </button>
